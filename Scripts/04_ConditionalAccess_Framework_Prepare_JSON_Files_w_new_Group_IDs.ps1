7718b383c58aa01e3d9ad3e8cb75cb9cd2528fbd
Philipp Kohn <philipp.kohn@outlook.com>
2023-08-13 11:51:10 +0200

    Use Readhost

--- Scripts/04_ConditionalAccess_Framework_Prepare_JSON_Files_w_new_Group_IDs.ps1
+++ Scripts/04_ConditionalAccess_Framework_Prepare_JSON_Files_w_new_Group_IDs.ps1
@@ -1,9 +1,9 @@
-<# 
-.SYNOPSIS 
+<#
+.SYNOPSIS
   This script updates the group IDs in the JSON files for conditional access policies to prepare them for Import
 
 
-.DESCRIPTION 
+.DESCRIPTION
   This script reads the source and target group mappings from two CSV files and creates a hashtable to store the old and new ID values. Then it loops through each JSON file in a folder and replaces the group IDs in the conditions.users.excludeGroups and conditions.users.includeGroups properties with the corresponding values from the hashtable. Finally, it overwrites the JSON files with the updated content.
 
 .OUTPUTS
@@ -18,75 +18,55 @@
 if ($PSVersionTable.PSVersion.Major -lt 7) {
   Throw "This script requires PowerShell 7 or a newer version."
 }
+# Ask for the source and target group mappings file path
+$FilePath = Read-Host -Prompt "Enter the file path for the source and target group mappings CSV files"
 
 # Get the source and target group mappings from the CSV files
-$SourceGroups = Import-Csv -Path "C:\Scripts\Conditional_Access_Framework_Groups_w_ID_Source.csv"
-$TargetGroups = Import-Csv -Path "C:\Scripts\Conditional_Access_Framework_Groups_w_ID_Target.csv"
-
-# Ask the user to confirm if they have checked or changed the file path to the CSV for the mapping table
-$Confirm = Read-Host -Prompt "Have you checked or changed, if necessary, the file path to the CSV-Files for the mapping table first? (y/n)"
-# If the user answers yes, proceed with the script
-if ($Confirm -eq "y") {
-    # Do something with the source and target groups
-}
-# If the user answers no, exit the script
-elseif ($Confirm -eq "n") {
-    Write-Host "Please check or change the file path to the CSV-Files for the mapping table first and then run the script again." -ForegroundColor Cyan
-    Exit
-}
-# If the user answers anything else, display an error message and exit the script
-else {
-    Write-Host "Invalid input. Please enter y or n." -ForegroundColor Red
-    Exit
-}
+$SourceGroups = Import-Csv -Path "$FilePath\Conditional_Access_Framework_Groups_w_ID_Source.csv"
+$TargetGroups = Import-Csv -Path "$FilePath\Conditional_Access_Framework_Groups_w_ID_Target.csv"
 
 # Create a hashtable to store the old and new ID values
 $GroupMap = @{}
 foreach ($i in 0..($SourceGroups.Count-1)) {
+  # For each pair of source and target groups, add their IDs to the hashtable
   $GroupMap[$SourceGroups[$i].Id] = $TargetGroups[$i].Id
 }
 
-# Load the assemblies that contain the types needed for the folder browser dialog
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+# Ask for the JSON files folder path
+$JsonFilesPath = Read-Host -Prompt "Enter the folder path for the JSON files"
 
-Write-Warning "Enter the path to folder with the exported Conditional Access Policies"
-$JsonFiles = Read-Host
-if ($JsonFiles) {
-    
-      # Loop through each JSON file
-  foreach ($JsonFile in $JsonFiles) {
-    # Convert the JSON content to a PowerShell object
-    $JsonContent = Get-Content -Path $JsonFile.FullName -Raw | ConvertFrom-Json
+# Get the JSON files from the folder
+$JsonFiles = Get-ChildItem -Path "$JsonFilesPath" -Filter "*.json"
 
-    # Loop through each excludeGroup value in the conditions.users.excludeGroups property
-    for ($i = 0; $i -lt $JsonContent.conditions.users.excludeGroups.Count; $i++) {
-      $ExcludeGroup = $JsonContent.conditions.users.excludeGroups[$i]
+# Loop through each JSON file
+foreach ($JsonFile in $JsonFiles) {
+  # Convert the JSON content to a PowerShell object
+  $JsonContent = Get-Content -Path $JsonFile.FullName -Raw | ConvertFrom-Json
 
-      # Check if the excludeGroup value exists in the hashtable keys
-      if ($GroupMap.ContainsKey($ExcludeGroup)) {
-        # Replace the excludeGroup value with the corresponding hashtable value
-        $JsonContent.conditions.users.excludeGroups[$i] = $GroupMap[$ExcludeGroup]
-      }
+  # Loop through each excludeGroup value in the conditions.users.excludeGroups property
+  for ($i = 0; $i -lt $JsonContent.conditions.users.excludeGroups.Count; $i++) {
+    $ExcludeGroup = $JsonContent.conditions.users.excludeGroups[$i]
+
+    # Check if the excludeGroup value exists in the hashtable keys
+    if ($GroupMap.ContainsKey($ExcludeGroup)) {
+      # Replace the excludeGroup value with the corresponding hashtable value
+      $JsonContent.conditions.users.excludeGroups[$i] = $GroupMap[$ExcludeGroup]
     }
-
-    # Loop through each includeGroup value in the conditions.users.includeGroups property
-    for ($i = 0; $i -lt $JsonContent.conditions.users.includeGroups.Count; $i++) {
-      $IncludeGroup = $JsonContent.conditions.users.includeGroups[$i]
-
-      # Check if the includeGroup value exists in the hashtable keys
-      if ($GroupMap.ContainsKey($IncludeGroup)) {
-        # Replace the includeGroup value with the corresponding hashtable value
-        $JsonContent.conditions.users.includeGroups[$i] = $GroupMap[$IncludeGroup]
-      }
-    }
-
-    # Convert the PowerShell object back to JSON and overwrite the file
-    $JsonContent | ConvertTo-Json -Depth 4 | Set-Content -Path $JsonFile.FullName
   }
-} else {
-    Write-Warning "No Path entered, aborting the script"
-    exit
+
+  # Loop through each includeGroup value in the conditions.users.includeGroups property
+  for ($i = 0; $i -lt $JsonContent.conditions.users.includeGroups.Count; $i++) {
+    $IncludeGroup = $JsonContent.conditions.users.includeGroups[$i]
+
+    # Check if the includeGroup value exists in the hashtable keys
+    if ($GroupMap.ContainsKey($IncludeGroup)) {
+      # Replace the includeGroup value with the corresponding hashtable value
+      $JsonContent.conditions.users.includeGroups[$i] = $GroupMap[$IncludeGroup]
+    }
+  }
+
+  # Convert the PowerShell object back to JSON and overwrite the file
+  $JsonContent | ConvertTo-Json -Depth 4 | Set-Content -Path $JsonFile.FullName
 }
 
 Write-Host ""

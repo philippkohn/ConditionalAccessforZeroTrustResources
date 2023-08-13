40f690932478fa57faa4a5d054f41df725c4ec9e
Philipp Kohn <philipp.kohn@outlook.com>
2023-08-13 12:04:19 +0200

    Initial commit

---
+++ Scripts/05_ConditionalAccess_Framework_Import_Policies.ps1
@@ -0,0 +1,76 @@
+<#
+.SYNOPSIS 
+    Exports all Conditional Access policies from a tenant to separate JSON files.
+
+.DESCRIPTION
+    This script connects to the Microsoft Graph API and retrieves all Conditional Access policies from a tenant. It then creates a folder named after the built-in onmicrosoft.com domain name of the tenant and the date of the export, and exports each policy to a separate JSON file with its actual name. It also displays a summary of the exported policies in the shell.
+
+.OUTPUTS
+    A folder containing JSON files for each Conditional Access policy, and a summary table in the shell.
+
+.NOTES
+    Author        Philipp Kohn, cloudcopilot.de, Twitter: @philipp_kohn
+    Change Log    V1.00, 13/08/2023 - Initial version
+    
+#>
+
+# Check PowerShell Version
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Throw "This script requires PowerShell 7 or a newer version."
+}
+
+# Try Discconnect Microsoft Graph API
+Write-Host "Disconnect from existing Microsoft Graph API Sessions"
+try{Disconnect-MgGraph -force -ErrorAction SilentlyContinue}catch{}
+
+# Connect to Microsoft Graph API
+Write-Host "Connecting to Microsoft Graph API..."
+$RequiredScopes = @('User.Read.All', 'Group.Read.All', 'Policy.ReadWrite.All')
+Write-Warning "Enter the Tenant ID of the tenant you want to connect to or leave blank to cancel"
+$TenantID = Read-Host
+if ($TenantID) {
+    Connect-MgGraph -Scopes $RequiredScopes -TenantId $TenantID -ErrorAction Stop
+} else {
+    Write-Warning "No Tenant ID entered, aborting the script"
+    exit
+}
+
+# Get the built-in onmicrosoft.com domain name of the tenant
+Write-Host "Getting the built-in onmicrosoft.com domain name of the tenant..."
+$tenantName = (Get-MgOrganization).VerifiedDomains | Where-Object {$_.IsInitial -eq $true} | Select-Object -ExpandProperty Name
+$CurrentUser = (Get-MgContext | Select-Object -ExpandProperty Account)
+Write-Host "Tenant: $tenantName" -ForegroundColor 'Magenta'
+Write-Host "User: $CurrentUser" -ForegroundColor 'Cyan'
+Write-Warning "Press any key to continue or Ctrl+C to cancel"
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+# Query for the path of the folder that contains the JSON files
+$path = Read-Host "Enter the path of the folder that contains the Conditional Access Template Files in the JSON format"
+
+# Get all JSON files from the folder
+Write-Host "Getting all JSON files from the folder..."
+$files = Get-ChildItem -Path $path -Filter *.json
+
+# Import all Conditional Access policies from JSON files and display a summary of the imported policies in the shell
+Write-Host "Importing all Conditional Access policies from JSON files and displaying a summary of the imported policies in the shell..."
+$summary = @()
+foreach ($file in $files) {
+    # Read the policy object from the JSON file
+    $policy = Get-Content -Path $file.FullName | ConvertFrom-Json
+    # Invoke a POST request to create the policy in the target tenant
+    $result = Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies -Body $policy
+    $summary += [PSCustomObject]@{
+        Name = $policy.DisplayName
+        Id = $result.Id
+        File = $file.FullName
+    }
+}
+
+#Disconnect Microsoft Graph API
+Write-Host "Disconnect from existing Microsoft Graph API Sessions" -ForegroundColor Cyan
+Disconnect-MgGraph
+
+Write-Host ""
+$summary | Format-Table -AutoSize
+Write-Host "Imported all Conditional Access policies to $($tenantName) from $($path)" -ForegroundColor Magenta
+Write-Host "Done."
